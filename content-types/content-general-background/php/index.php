f8b56cbff0fb6688896f45462750456d1eb4e1ca
Sam Poon <spoon6@fordham.edu>
2024-10-09 16:08:42 -0400

    content-general-background: added text/html to codebase

--- content-types/content-general-background/php/index.php
+++ content-types/content-general-background/php/index.php
@@ -4,7 +4,7 @@
   $background_layer = "<t4 type='content' name='Background layer' output='normal' formatter='path/*' />";
   $heading = "<t4 type='content' name='Heading' output='normal' modifiers='striptags,htmlentities' />";
 ?>
-<!-- > -->
+
 <section class="general-content background-image <?php echo $top_bottom_spacing; ?> <?php echo $limit_width; ?>" style="background-image: url(<?php echo $background_layer; ?>);">
   <t4 type="meta" meta="html_anchor" />
   <div class="general-content__block <?php echo $heading; ?>">
