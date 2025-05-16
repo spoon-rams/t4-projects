039e1251220e34c061e84fa5f4d53e105db2cf82
Sam Poon <spoon6@fordham.edu>
2025-05-16 15:03:17 -0400

    scroll-fade: updates to a fn to an arrow fn

--- content-types/scroll-fade/script.js
+++ content-types/scroll-fade/script.js
@@ -39,7 +39,7 @@
 //   window.addEventListener("scroll", checkFadeIns);
 // });
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
   const sections = document.querySelectorAll("section");
 
   function scrollAnimations() {
