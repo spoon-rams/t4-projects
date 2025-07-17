558e7dc919f5311f2e2e545a55f976fbf5dab42c
spoon-rams <spoon6@fordham.edu>
2025-07-17 12:49:57 -0400

    rebrand-navigation: created css for navigation, created js for navigation feature, minor css updates for navigation feature in js

--- content-types/current/video-hero/js/script.js
+++ content-types/current/video-hero/js/script.js
@@ -1,3 +1,4 @@
+console.log("CONNECTED!");
 document.addEventListener("DOMContentLoaded", () => {
   const video = document.getElementById("hero-video");
   const playPauseBtn = document.querySelector(".play-pause-btn");
@@ -20,6 +21,37 @@ document.addEventListener("DOMContentLoaded", () => {
     } else {
       video.pause();
       playPauseBtn.innerHTML = playback.play;
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const mainNav = document.querySelector(".header-nav__bottom");
+  const fillStart = 0; // starts (at the very top)
+  const fillEnd = 200; // fill is complete (200px down)
+  let windowSize = window.innerWidth;
+
+
+  window.addEventListener("resize", () => {
+    windowSize = window.innerWidth;
+    // Removes the style attribute so that the mobile menu is visible
+    if (windowSize < 1279) {
+      mainNav.removeAttribute("style");
+    }
+  });
+
+  window.addEventListener("scroll", () => {
+    let scrollY = window.scrollY;
+    if (windowSize > 1279 && scrollY > 2) {
+      // Calculate the progress of the scroll within the fill range
+      let progress = (scrollY - fillStart) / (fillEnd - fillStart);
+
+      // Clamp the progress between 0 and 1
+      progress = Math.max(0, Math.min(1, progress));
+
+      // Fades in and fades out the main navigation background color and border top of the navigation
+      mainNav.style.backgroundColor = `rgba(126,7,42, ${progress})`;
+      mainNav.style.borderTop = `1px solid rgba(255, 255, 255, ${progress})`;
     }
   });
 });
