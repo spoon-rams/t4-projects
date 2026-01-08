f2cf286e2a97b0bbc67e532c61ba89a718f05766
spoon-rams <spoon6@fordham.edu>
2026-01-08 15:45:30 -0500

    hero-carousel-2026: added fixes to the js file for data render when clicking on indicators, css fixes to changed arrow class name

--- content-types/current/hero-banner-carousel/js/script.js
+++ content-types/current/hero-banner-carousel/js/script.js
@@ -2,12 +2,13 @@ document.addEventListener("DOMContentLoaded", () => {
   const slidesContainer = document.querySelector(".carousel-header .slide-container");
   const slide = document.querySelector(".carousel-header .slide");
   const slides = document.querySelectorAll(".carousel-header .slide");
-  const prevButton = document.querySelector(".carousel-header .carousel-arrow.prev");
-  const nextButton = document.querySelector(".carousel-header .carousel-arrow.next");
+  const prevButton = document.querySelector(".carousel-header .hero-carousel-arrow.prev");
+  const nextButton = document.querySelector(".carousel-header .hero-carousel-arrow.next");
   const indicatorParent = document.querySelector(".carousel-header .carousel-indicators");
 
+  const contentBox = document.querySelector(".carousel-header .img-container .content");
   const contentTitle = document.querySelector(".carousel-header .content .title");
-  const contentButtonText = document.querySelector(".carousel-header .content .button-text span");
+  const contentButtonText = document.querySelector(".carousel-header .content .button-text");
 
   let startX = 0;
   let clickPending = false;
@@ -54,8 +55,6 @@ document.addEventListener("DOMContentLoaded", () => {
       e.stopImmediatePropagation();
       return;
     }
-    contentTitle.innerText = slides[currentSlide + 1].dataset.title;
-    contentButtonText.innerText = slides[currentSlide + 1].dataset.buttonText;
 
     clickPending = true;
     handleClick("next", slidesContainer, slide);
@@ -67,8 +66,7 @@ document.addEventListener("DOMContentLoaded", () => {
       e.stopImmediatePropagation();
       return;
     }
-    contentTitle.innerText = slides[currentSlide - 1].dataset.title;
-    contentButtonText.innerText = slides[currentSlide - 1].dataset.buttonText;
+
     clickPending = true;
     handleClick("previous", slidesContainer, slide);
     setTimeout(() => (clickPending = false), 800);
@@ -102,47 +100,6 @@ document.addEventListener("DOMContentLoaded", () => {
     startX = e.touches[0].clientX;
   });
 
-  document.querySelector(".carousel-header .img-container .content").addEventListener("touchstart", (e) => {
-    startX = e.touches[0].clientX;
-  });
-
-  document.querySelector(".carousel-header .img-container .content").addEventListener("touchend", (e) => {
-    var endX = e.changedTouches[0].clientX;
-    var deltaX = startX - endX;
-
-    // Set a threshold for swipe distance
-    var swipeThreshold = 50;
-
-    if (deltaX > swipeThreshold) {
-      // Swipe left, scroll to the next box
-      if (currentSlide < slides.length - 1) {
-        contentTitle.innerText = slides[currentSlide + 1].dataset.title;
-        contentButtonText.innerText = slides[currentSlide + 1].dataset.buttonText;
-        currentSlide++;
-        
-        setIndicator(currentSlide);
-      }
-    } else if (deltaX < -swipeThreshold) {
-      // Swipe right, scroll to the previous box
-      if (currentSlide > 0) {
-        contentTitle.innerText = slides[currentSlide - 1].dataset.title;
-        contentButtonText.innerText = slides[currentSlide - 1].dataset.buttonText;
-        currentSlide--;
-        
-        setIndicator(currentSlide);
-      }
-    }
-
-    // Calculate the scroll position based on the current index
-    var scrollPosition = slides[currentSlide].offsetLeft - slidesContainer.offsetLeft;
-
-    // Scroll to the selected box with a smooth animation
-    slidesContainer.scrollTo({
-      left: scrollPosition,
-      behavior: "smooth",
-    });
-  });
-
   slidesContainer.addEventListener("touchend", (e) => {
     var endX = e.changedTouches[0].clientX;
     var deltaX = startX - endX;
@@ -174,10 +131,50 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 
+  contentBox.addEventListener("touchstart", (e) => {
+    startX = e.touches[0].clientX;
+  });
+
+  contentBox.addEventListener("touchend", (e) => {
+    var endX = e.changedTouches[0].clientX;
+    var deltaX = startX - endX;
+
+    // Set a threshold for swipe distance
+    var swipeThreshold = 50;
+
+    if (deltaX > swipeThreshold) {
+      // Swipe left, scroll to the next box
+      if (currentSlide < slides.length - 1) {
+        contentTitle.innerText = slides[currentSlide + 1].dataset.title;
+        contentButtonText.innerText = slides[currentSlide + 1].dataset.buttonText;
+        currentSlide++;
+        setIndicator(currentSlide);
+      }
+    } else if (deltaX < -swipeThreshold) {
+      // Swipe right, scroll to the previous box
+      if (currentSlide > 0) {
+        contentTitle.innerText = slides[currentSlide - 1].dataset.title;
+        contentButtonText.innerText = slides[currentSlide - 1].dataset.buttonText;
+        currentSlide--;
+        setIndicator(currentSlide);
+      }
+    }
+
+    // Calculate the scroll position based on the current index
+    var scrollPosition = slides[currentSlide].offsetLeft - slidesContainer.offsetLeft;
+
+    // Scroll to the selected box with a smooth animation
+    slidesContainer.scrollTo({
+      left: scrollPosition,
+      behavior: "smooth",
+    });
+    // swipeAction(e, currentSlide, slides, slidesContainer, contentTitle, contentButtonText);
+  });
+
   // HELPER FUNCTION - SETS THE INDICATOR DOT - Refactoring
   function setIndicator(index) {
-    const prevButton = document.querySelector(".carousel-arrow.prev");
-    const nextButton = document.querySelector(".carousel-arrow.next");
+    const prevButton = document.querySelector(".hero-carousel-arrow.prev");
+    const nextButton = document.querySelector(".hero-carousel-arrow.next");
     const slides = document.querySelectorAll(".slide");
     const indicators = document.querySelectorAll(".carousel-indicator");
 
@@ -210,16 +207,22 @@ document.addEventListener("DOMContentLoaded", () => {
     // const slideWidth = slide.clientWidth;
     switch (action) {
       case "next":
+        contentTitle.innerText = slides[currentSlide + 1].dataset.title;
+        contentButtonText.innerText = slides[currentSlide + 1].dataset.buttonText;
         container.scrollLeft += slide.clientWidth;
         currentSlide += 1;
         return setIndicator(currentSlide);
       case "previous":
+        contentTitle.innerText = slides[currentSlide - 1].dataset.title;
+        contentButtonText.innerText = slides[currentSlide - 1].dataset.buttonText;
         container.scrollLeft -= slide.clientWidth;
         currentSlide -= 1;
         return setIndicator(currentSlide);
       case "indicator":
         container.scrollLeft = index * slide.clientWidth;
         currentSlide = index;
+        contentTitle.innerText = slides[currentSlide].dataset.title;
+        contentButtonText.innerText = slides[currentSlide].dataset.buttonText;
         return setIndicator(index);
       case "resize":
         container.scrollLeft = currentSlide * slide.clientWidth;
