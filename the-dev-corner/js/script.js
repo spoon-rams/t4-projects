9b08e7dd5d837b8aae1df4b96400f3088acd5aee
spoon-rams <spoon6@fordham.edu>
2026-07-01 09:44:35 -0400

    the-dev-corner: added accordion, sub menu and sub sub menu option to the project

--- the-dev-corner/js/script.js
+++ the-dev-corner/js/script.js
@@ -1,9 +1,25 @@
 const accordions = document.querySelectorAll(".accordion");
+const submenuToggles = document.querySelectorAll(".submenu-toggle");
 
 accordions.forEach((button) => {
   button.addEventListener("click", () => {
     const navItem = button.parentElement;
-    navItem.classList.toggle("open");
+    if (navItem.classList.contains("open")) {
+      navItem.classList.remove("open");
+    } else {
+      const openItem = document.querySelector(".nav-item.open");
+      if (openItem) {
+        openItem.classList.remove("open");
+      }
+      navItem.classList.add("open");
+    }
+  });
+});
+
+submenuToggles.forEach((button) => {
+  button.addEventListener("click", () => {
+    const submenuItem = button.parentElement;
+    submenuItem.classList.toggle("open");
   });
 });
 
